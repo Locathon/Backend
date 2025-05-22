40c608fdde40833320934ea64eaab025feb1926c
cylin0201 <cylin0201@naver.com>
2025-05-22 23:07:12 +0900

    first commit

---
+++ src/main/java/com/Locathon/LocathonApplication.java
@@ -0,0 +1,13 @@
+package com.Locathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Locathon/LocathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Locathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
